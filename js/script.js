ae30dec423c0a874b00d79db0f6efc5d1ee20a73
Faithnwogu <dfaytech@gmail.com>
2023-09-25 12:39:50 +0100

    index,services and tracking page done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const mobile_icon = document.getElementById('mobile_icon');
+const mobileLinks = document.getElementById('mobile_links');
+
+
+
+mobile_icon.addEventListener('click', ()=>{
+      mobileLinks.classList.toggle('toggle')
+})
+
+
+const preloader = document.getElementById('preloader');
+console.log(preloader)
